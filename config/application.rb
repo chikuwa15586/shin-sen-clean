1ec1eaa30c0bc8e28e313f89de819a1d64f45c55
mgamo <m.gamo012@outlook.jp>
2026-03-18 17:25:45 +0900

    fix: remove NOT NULL constraint for fortune_id

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ module ShinSenClean
     config.autoload_lib(ignore: %w[assets tasks])
 
     config.i18n.default_locale = :ja
+
+    config.i18n.available_locales = [:ja, :en]
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
