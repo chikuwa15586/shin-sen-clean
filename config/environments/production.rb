c6a92fb1d235eda50d364258d540937273d22778
mgamo <m.gamo012@outlook.jp>
2026-03-19 15:53:21 +0900

    fix: consider true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   #   port: 587,
   #   authentication: :plain
   # }
-
+  config.consider_all_requests_local = true
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
